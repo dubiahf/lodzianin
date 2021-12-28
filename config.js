a90e8f2bb46ea684d1e57d6a4c6275f3df15fed4
dubiahf <76068508+dubiahf@users.noreply.github.com>
2021-12-28 12:17:14 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
-    consumer_key:         'c62wRwipXDqjv6LupEjy08CgP',
-    consumer_secret:      'AomA10G2a4wvBvrM1TuEoxcGSbc0FUBxLpPI7TI8VrOfneBtFL',
-    access_token:         '758466634298302464-5CLHymTksKMyCFb7WgDVvvrx2qqwc5E',
-    access_token_secret:  'ZOP4V4w4XVSskhDE4Br5zYRLlPZ1dOxxjNC4m2dnKT2Fu'
+    consumer_key:         '',
+    consumer_secret:      '',
+    access_token:         '',
+    access_token_secret:  ''
     }
   
 module.exports = config;
